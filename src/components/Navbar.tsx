3c2415a5b969e9fef9ee57c7c608cd900e50da61
Dartths <philipcandon@live.com>
2025-10-19 00:38:36 +0100

    updating readmore buttons and adding responsive task navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,11 @@
 import { Link } from 'react-router-dom';
 import images from '../assets/images';
+import { useState } from 'react';
+import { Transition } from '@headlessui/react';
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <nav className="bg-primary text-white">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
@@ -33,8 +37,103 @@ const Navbar = () => {
               </Link>
             </div>
           </div>
+          <div className="-mr-2 flex md:hidden">
+            <button
+              onClick={() => setIsOpen(!isOpen)}
+              type="button"
+              className="bg-primary inline-flex items-center justify-center p-2 rounded-md text-white hover:text-white hover:bg-secondary focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-primary focus:ring-white"
+              aria-controls="mobile-menu"
+              aria-expanded="false"
+            >
+              <span className="sr-only">Open main menu</span>
+              {!isOpen ? (
+                <svg
+                  className="block h-6 w-6"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  aria-hidden="true"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M4 6h16M4 12h16M4 18h16"
+                  />
+                </svg>
+              ) : (
+                <svg
+                  className="block h-6 w-6"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  aria-hidden="true"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M6 18L18 6M6 6l12 12"
+                  />
+                </svg>
+              )}
+            </button>
+          </div>
         </div>
       </div>
+
+      <Transition
+        show={isOpen}
+        enter="transition ease-out duration-100 transform"
+        enterFrom="opacity-0 scale-95"
+        enterTo="opacity-100 scale-100"
+        leave="transition ease-in duration-75 transform"
+        leaveFrom="opacity-100 scale-100"
+        leaveTo="opacity-0 scale-95"
+      >
+        <div className="md:hidden" id="mobile-menu">
+          <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
+            <Link
+              to="/"
+              className="text-white hover:bg-secondary block px-3 py-2 rounded-md text-base font-medium font-title"
+            >
+              Home
+            </Link>
+            <Link
+              to="/about"
+              className="text-white hover:bg-secondary block px-3 py-2 rounded-md text-base font-medium font-title"
+            >
+              About
+            </Link>
+            <Link
+              to="/clubs-and-organisations"
+              className="text-white hover:bg-secondary block px-3 py-2 rounded-md text-base font-medium font-title"
+            >
+              Clubs and Organisations
+            </Link>
+            <Link
+              to="/things-to-do"
+              className="text-white hover:bg-secondary block px-3 py-2 rounded-md text-base font-medium font-title"
+            >
+              Things to Do
+            </Link>
+            <Link
+              to="/latest-news"
+              className="text-white hover:bg-secondary block px-3 py-2 rounded-md text-base font-medium font-title"
+            >
+              Latest News
+            </Link>
+            <Link
+              to="/contact"
+              className="text-white hover:bg-secondary block px-3 py-2 rounded-md text-base font-medium font-title"
+            >
+              Contact Us
+            </Link>
+          </div>
+        </div>
+      </Transition>
     </nav>
   );
 };
