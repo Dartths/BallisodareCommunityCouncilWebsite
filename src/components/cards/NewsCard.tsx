3a7d9de0cc24002139695acbc0f6a908ae9ab8fb
Molly Candon <molly@hrlocker.com>
2025-11-22 22:33:24 +0000

    adding images to newscards

--- src/components/cards/NewsCard.tsx
+++ src/components/cards/NewsCard.tsx
@@ -5,13 +5,15 @@ interface NewsCardProps {
   title: string;
   description: string;
   link: string;
+  image:any;
 }
 
-const NewsCard: React.FC<NewsCardProps> = ({ title, description, link }) => {
+const NewsCard: React.FC<NewsCardProps> = ({ title, description, link, image }) => {
   return (
     <div className="bg-white rounded-lg shadow-md hover:shadow-lg transition-shadow overflow-hidden">
       <div className="p-6">
         <h3 className="text-xl font-semibold mb-3 font-title text-secondary">{title}</h3>
+        <img src={image?.src} alt={image?.alt}></img>
         <p className="text-gray-700 mb-4 font-body">{description}</p>
            <div className="text-center">
             <ReadMoreButton href={link} text="Read More" />
