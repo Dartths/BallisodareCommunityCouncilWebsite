25efe596cd94dcc5696b5de11becc1f84dfe7dfc
Dartths <philipcandon@live.com>
2026-02-08 17:32:28 +0000

    making new icon components

--- src/components/Layout/Navbar.tsx
+++ src/components/Layout/Navbar.tsx
@@ -4,6 +4,8 @@ import { useState } from 'react';
 import { Transition } from '@headlessui/react';
 import NavbarSubmenu from './NavbarSubmenu';
 import { home, about, facilities, clubs, activities, news, contact, heritage } from '../../config/routes';
+import HamburgerIcon from '../UI/HamburgerIcon';
+import CloseIcon from '../UI/CloseIcon';
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -57,37 +59,9 @@ const Navbar = () => {
             >
               <span className="sr-only">Open main menu</span>
               {!isOpen ? (
-                <svg
-                  className="block h-6 w-6"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                  aria-hidden="true"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M4 6h16M4 12h16M4 18h16"
-                  />
-                </svg>
+                <HamburgerIcon  />
               ) : (
-                <svg
-                  className="block h-6 w-6"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                  aria-hidden="true"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M6 18L18 6M6 6l12 12"
-                  />
-                </svg>
+                <CloseIcon />
               )}
             </button>
           </div>
