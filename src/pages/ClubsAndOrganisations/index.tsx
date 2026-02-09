27a1aa8a83df928264f901d56b996cbf31585cb9
Darrths <philipcandon@live.com>
2026-02-09 19:53:57 +0000

    reording and fixing display positions

--- src/pages/ClubsAndOrganisations/index.tsx
+++ src/pages/ClubsAndOrganisations/index.tsx
@@ -4,12 +4,12 @@ import ImageTextSection from "../../components/sections/ImageTextSection";
 const ClubsAndOrganisations = () => {
   return (
      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
-        <ImageTextSection {...ArdVoltasContent} />
         <ImageTextSection {...MensShedContent} imageLeft={false} />
         <ImageTextSection {...WomensShedContent} />
         <ImageTextSection {...ScoutsContent} imageLeft={false}/>
-        <ImageTextSection {...TidyTownsContent}imageLeft={false}/>
-        <ImageTextSection {...FishingClubContent} />
+        <ImageTextSection {...TidyTownsContent} />
+        <ImageTextSection {...FishingClubContent}  imageLeft={false}/>
+        <ImageTextSection {...ArdVoltasContent} />
         {/* <ImageTextSection {...ScoutsContent} imageLeft={false}/> */}
     </div>
   );
